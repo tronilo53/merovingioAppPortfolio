d618b16161f2f1e1e715414d4bea623b1840777b
tronilo53 <tronilo53@gmail.com>
2022-08-28 23:12:19 +0200

    interacción en nav acabada, scrollspy terminado y libreria AOS(animaciones js) instalada bajo npm

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/shared/header/header.component';
 import { SectionComponent } from './components/shared/section/section.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    SectionComponent
+    SectionComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
